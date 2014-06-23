eaf8b189f9bd866892aea05903cdcd8236452e57
doragon <doragon@users.noreply.github.com>
2014-06-23 11:08:42 +0900

    Update export.sh

--- ubuntu/go/export.sh
+++ ubuntu/go/export.sh
@@ -1,3 +1,4 @@
 GOROOT=/usr/local/go
 GOPATH=$HOME/gocode
 PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+RUN curl -o /usr/local/src/export.sh https://raw.githubusercontent.com/doragon/Dockerfile/master/ubuntu/go/export.sh
